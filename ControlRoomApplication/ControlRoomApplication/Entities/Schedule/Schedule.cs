b9fab96b70449c02731c7546fd8f9f52f61683d9
Jason Porter <jporter7@ycp.edu>
2018-10-08 16:27:23 -0400

    Creating the schedule entity.

---
+++ ControlRoomApplication/ControlRoomApplication/Entities/Schedule/Schedule.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ControlRoomApplication.Entities.Schedule
+{
+    [Table("Schedule")]
+    public class Schedule
+    {
+        public Schedule()
+        {
+
+        }
+
+        public virtual ICollection<Appointment> Appointments { get; set; }
+    }
+}
