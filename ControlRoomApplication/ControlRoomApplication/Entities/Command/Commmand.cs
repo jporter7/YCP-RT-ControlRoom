ffa6d79e26461281dc0c4d8b589c9d1f0cdbee5b
ruthsgarrett@gmail.com <Spaz:)37>
2018-10-10 15:51:59 -0400

    Initial Commit of Command Entity

---
+++ ControlRoomApplication/ControlRoomApplication/Entities/Command/Commmand.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ControlRoomApplication.Entities.Command
+{
+    class Commmand
+    {
+
+        public double Resolution { get; set; }
+
+        public double T_Move { get; set; }
+
+        [Required]
+        [Column("start_time")]
+        public DateTime StartTime { get; set; }
+
+        [Required]
+        [Column("end_time")]
+        public DateTime EndTime { get; set; }
+        
+    }
+}
