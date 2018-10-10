f36fcfd80cc6f35bfe1904b2ebc9a7b08f914f26
Jason Porter <jporter7@ycp.edu>
2018-10-10 12:46:39 -0400

    Creating the appointment branch, commenting the appointment class, and creating the appointment tests.

--- ControlRoomApplication/ControlRoomApplication/Entities/Appointment/Appointment.cs
+++ ControlRoomApplication/ControlRoomApplication/Entities/Appointment/Appointment.cs
@@ -4,26 +4,41 @@ using System.ComponentModel.DataAnnotations.Schema;
 
 namespace ControlRoomApplication.Entities
 {
+    /// <summary>
+    /// This class will model the Appointment structure in our system.
+    /// The Appointment entity will use a radio-telescope between the designated
+    /// start time and end time and will be created by users.
+    /// </summary>
     [Table("Appointments")]
     public class Appointment
     {
+        /// <summary>
+        /// This is the empty constructor for the Appointment entity.
+        /// </summary>
         public Appointment()
         {
-
+            Id = -9999;
+            StartTime = new DateTime();
+            EndTime = new DateTime();
         }
 
+        /// <summary>
+        /// The getter/setter for the unique Id field associated with this Appointment.
+        /// </summary>
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
-        //[Required]
-        //[Column("user_id")]
-        //public virtual int UserId { get; set; }
-
+        /// <summary>
+        /// The getter/setter for the start time associated with this Appointment.
+        /// </summary>
         [Required]
         [Column("start_time")]
         public DateTime StartTime { get; set; }
 
+        /// <summary>
+        /// The getter/setter for the end time associated with this Appointment.
+        /// </summary>
         [Required]
         [Column("end_time")]
         public DateTime EndTime { get; set; }
---
+++ ControlRoomApplication/ControlRoomApplicationTest/EntitiesTests/AppointmentTest.cs
@@ -0,0 +1,35 @@
+﻿using ControlRoomApplication.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ControlRoomApplicationTest.EntitiesTests
+{
+    [TestClass]
+    public class AppointmentTest
+    {
+        // Class being tested
+        private Appointment appointment;
+
+        // Attributes in the class that need to be tested
+        private DateTime startTime;
+        private DateTime endTime;
+
+        [TestInitialize]
+        public void BuildUp()
+        {
+            // Initialize appointment entity
+            appointment = new Appointment();
+
+            // Initialize fields we are testing against
+            startTime = new DateTime();
+            endTime = new DateTime();
+        }
+
+        [TestMethod]
+        public void TestGettersAndSetters()
+        {
+            Assert.AreEqual(startTime.Year, appointment.StartTime.Year);
+            Assert.AreEqual(endTime.Year, appointment.EndTime.Year);
+        }
+    }
+}
