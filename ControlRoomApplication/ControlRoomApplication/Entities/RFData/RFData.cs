8d1c0fa5a2db27ffa34cecfcf28542ff347e10de
Nick Vandemark <rvandemark@ycp.edu>
2018-10-11 01:33:14 -0400

    Initial structure for RFData entity.

---
+++ ControlRoomApplication/ControlRoomApplication/Entities/RFData/RFData.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ControlRoomApplication.Entities
+{
+    public class RFData
+    {
+        public RFData()
+        {
+
+        }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("time_captured")]
+        public DateTime TimeCaptured { get; set; }
+
+        [Required]
+        [Column("intensity")]
+        public long Intensity { get; set; }
+
+        public virtual Orientation AcquisitionOrientation { get; set; }
+    }
+}
