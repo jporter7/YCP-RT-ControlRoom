a624556ea989d706d4bd8e84dec5edd231399155
Nick Vandemark <rvandemark@ycp.edu>
2018-10-11 01:24:02 -0400

    Initial structure for Orientation entity.

---
+++ ControlRoomApplication/ControlRoomApplication/Entities/Orientation/Orientation.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ControlRoomApplication.Entities
+{
+    public class Orientation
+    {
+        public Orientation(long azimuth, long elevation)
+        {
+            Azimuth = azimuth;
+            Elevation = elevation;
+        }
+
+        public Orientation() : this(0, 0) { }
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
+
+        [Required]
+        [Column("azimuth")]
+        public long Azimuth { get; set; }
+
+        [Required]
+        [Column("elevation")]
+        public long Elevation { get; set; }
+    }
+}
