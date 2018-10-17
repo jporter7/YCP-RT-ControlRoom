84fcd768b722faae4ae0fd4c3bc8f078368b633e
Jason Porter <jporter7@ycp.edu>
2018-10-17 01:07:24 -0400

    Creating AppointmentController and adding some attributes.

---
+++ ControlRoomApplication/ControlRoomApplication/Controllers/AppointmentController/AppointmentController.cs
@@ -0,0 +1,28 @@
+﻿using ControlRoomApplication.Entities;
+using ControlRoomApplication.Main;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlRoomApplication.Controllers.AppointmentController
+{
+    public class AppointmentController
+    {
+        public AppointmentController(Appointment appointment, RTDbContext context)
+        {
+            Appointment = appointment;
+            Context = context;
+        }
+
+        public Boolean ValidateAppointment()
+        {
+
+            return false;
+        }
+
+        public Appointment Appointment { get; set; }
+        public RTDbContext Context { get; set; }
+    }
+}
