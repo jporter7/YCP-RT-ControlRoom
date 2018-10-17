650997ccb403c6aab359c9ae88df222f77627b91
Jason Porter <jporter7@ycp.edu>
2018-10-16 21:38:20 -0400

    Creating the ScheduleController class.

---
+++ ControlRoomApplication/ControlRoomApplication/Controllers/ScheduleController/ScheduleController.cs
@@ -0,0 +1,16 @@
+﻿using ControlRoomApplication.Entities;
+using System;
+using System.Collections.Generic;
+
+namespace ControlRoomApplication.Controllers.ScheduleController
+{
+    public class ScheduleController
+    {
+        public ScheduleController(Schedule schedule)
+        {
+            Schedule = schedule;
+        }
+
+        public Schedule Schedule { get; set; }
+    }
+}
