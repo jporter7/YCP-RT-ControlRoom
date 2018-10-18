77c8923cea2ec5a975d5dc7b4fd0616fc9464643
Nick Vandemark <rvandemark@ycp.edu>
2018-10-18 18:44:00 -0400

    Added Orientation tests.

---
+++ ControlRoomApplication/ControlRoomApplicationTest/EntitiesTests/OrientationTest.cs
@@ -0,0 +1,44 @@
+﻿using ControlRoomApplication.Entities;
+using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ControlRoomApplicationTest.EntitiesTests
+{
+    [TestClass]
+    public class OrientationTest
+    {
+        private Orientation orientation;
+
+        private long azimuth;
+        private long elevation;
+
+        [TestInitialize]
+        public void BuildUp()
+        {
+            orientation = new Orientation();
+
+            azimuth = 0;
+            elevation = 0;
+        }
+
+        [TestMethod]
+        public void TestInitialization()
+        {
+            Assert.AreEqual(azimuth, orientation.Azimuth);
+            Assert.AreEqual(elevation, orientation.Elevation);
+        }
+
+        [TestMethod]
+        public void TestSettersAndGetters()
+        {
+            azimuth = 10;
+            elevation = 25;
+
+            orientation.Azimuth = 10;
+            orientation.Elevation = 25;
+
+            Assert.AreEqual(azimuth, orientation.Azimuth);
+            Assert.AreEqual(elevation, orientation.Elevation);
+        }
+    }
+}
