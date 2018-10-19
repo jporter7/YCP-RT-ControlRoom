0278ca056ccbdba1a96f0bda7a1eec3a95d65b8a
Nick Vandemark <rvandemark@ycp.edu>
2018-10-18 23:03:27 -0400

    Added SpectraCyberController to FullRadioTelescope

--- ControlRoomApplication/ControlRoomApplication/Entities/RadioTelescope/RadioTelescope.cs
+++ ControlRoomApplication/ControlRoomApplication/Entities/RadioTelescope/RadioTelescope.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿using ControlRoomApplication.Controllers.SpectraCyberController;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
@@ -6,6 +6,7 @@ using System.ComponentModel.DataAnnotations.Schema;
 // Changes made here that need to be reflected in the UML (if agreed upon):
 //  - RadioTelescopeStatusEnum has Unknown, Moving, Integrating, and MovingAndIntegrating now
 //  - RadioTelescope has coordinates (InstallLocation) for where it physically is, so calculations that need that position have it available
+//  - Added the Simulated and FullRadioTelescope classes
 //
 
 namespace ControlRoomApplication.Entities
@@ -27,10 +28,11 @@ namespace ControlRoomApplication.Entities
 
         public RadioTelescopeStatusEnum CurrentStatus { get; set; }
 
-        public virtual Orientation CurrentOrientation { get; set; }
-
         // Add when Coordinate entity is ready
-        //public virtual Coordinate.Coordinate InstallLocation { get; set; }
+        //public virtual Coordinate InstallLocation { get; set; }
+
+        public abstract Orientation GetCurrentReferenceOrientation();
+        public abstract bool SendReferenceVelocityCommand(double velocityAzimuth, double velocityElevation);
     }
 
     public class SimulatedTelescope : RadioTelescope
@@ -39,15 +41,36 @@ namespace ControlRoomApplication.Entities
         {
             // Nothing extra at the moment
         }
+
+        public override Orientation GetCurrentReferenceOrientation()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool SendReferenceVelocityCommand(double velocityAzimuth, double velocityElevation)
+        {
+            throw new System.NotImplementedException();
+        }
     }
 
     public class FullTelescope : RadioTelescope
     {
-        private SpectraCyber FTSpectraCyber;
+        private SpectraCyberController FTSpectraCyberController;
 
         public FullTelescope()
         {
-            FTSpectraCyber = new SpectraCyber();
+            FTSpectraCyberController = new SpectraCyberController(new SpectraCyber());
+            FTSpectraCyberController.BringUp();
+        }
+
+        public override Orientation GetCurrentReferenceOrientation()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public override bool SendReferenceVelocityCommand(double velocityAzimuth, double velocityElevation)
+        {
+            throw new System.NotImplementedException();
         }
     }
 }
