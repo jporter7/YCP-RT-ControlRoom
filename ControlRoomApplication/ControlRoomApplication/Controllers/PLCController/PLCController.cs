f2551a24e16866a69f33b5febd93e97cb5a24409
Chris Garrety <cgarret3@ycp.edu>
2018-10-22 18:10:48 -0400

    PLC Controller Skeleton
    Skeleton added.  Currently throws Not Implemented exceptions

---
+++ ControlRoomApplication/ControlRoomApplication/Controllers/PLCController/PLCController.cs
@@ -0,0 +1,25 @@
+﻿namespace ControlRoomApplication.Controllers.PLCController
+{
+    class PLCController
+    {
+        public void CalibrateRT()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void ShutdownRT()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateAzimuth()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public void UpdateElevation()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
