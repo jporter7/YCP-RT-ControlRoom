f6fdb6036dbbb33c37027bfa0e47e3e371aa8509
cgarret3 <chris.garrety@gmail.com>
2018-11-01 09:40:50 -0400

    Added a simple server
    Grabs the local IP address, attaches port 8081 to create socket.  Listens for a List in the form of a TCP/IP String and prints out status

---
+++ ControlRoomApplication/ControlRoomApplication/Simulators/Server/Server.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace ControlRoomApplication.Simulators.Server
+{
+    class Server
+    {
+        public Server()
+        {
+            try
+            {
+                string localIP = Dns.GetHostEntry(Dns.GetHostName()).AddressList[0].ToString();
+                IPAddress ipAd = IPAddress.Parse(localIP);
+
+                // starts listener
+                // setting port to 8081 for YCP use
+                TcpListener myList = new TcpListener(ipAd, 8081);
+                myList.Start();
+
+                Console.WriteLine("Running on " + localIP + ":8081");
+                Console.WriteLine("Endpoint:" + myList.LocalEndpoint);
+                Console.WriteLine("Waiting for connection");
+
+                Socket s = myList.AcceptSocket();
+                Console.WriteLine("Connection Accepted");
+
+                // prepares to receive packet
+                byte[] b = new byte[100];
+                int k = s.Receive(b);
+                Console.WriteLine("Received...");
+                for (int i = 0; i < k; i++)
+                {
+                    Console.Write(Convert.ToChar(b[i]));
+                }
+
+                // sends acknowledgment
+                ASCIIEncoding asen = new ASCIIEncoding();
+                s.Send(asen.GetBytes("The string was received by the server."));
+                Console.WriteLine("\nSent ACK");
+
+                // clean up
+                s.Close();
+                myList.Stop();
+            }
+            catch (Exception exception)
+            {
+                Console.WriteLine("Error: " + exception.StackTrace);
+            }
+        }
+    }
+}
