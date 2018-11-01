462688e4239a2d040bcf01c37889bf2c24cdb781
cgarret3 <chris.garrety@gmail.com>
2018-11-01 09:48:49 -0400

    Added Unimplemented FrontEndSim/BackEndSim

---
+++ ControlRoomApplication/ControlRoomApplication/Simulators/BackEndSim/BackEndSim.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ControlRoomApplication.Simulators.BackEndSim
+{
+    class BackEndSim
+    {
+        public BackEndSim() { throw new NotImplementedException("TODO"); }
+    }
+}
