8d0e2180b988806ac8e20f7ec5f1f840f4b61243
cgarret3 <chris.garrety@gmail.com>
2018-11-01 10:02:37 -0400

    Added some Skeleton code for BackEnd

--- ControlRoomApplication/ControlRoomApplication/Simulators/BackEndSim/BackEndSim.cs
+++ ControlRoomApplication/ControlRoomApplication/Simulators/BackEndSim/BackEndSim.cs
@@ -1,10 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using ControlRoomApplication.Entities;
+using System;
+using System.Collections.ObjectModel;
 
 namespace ControlRoomApplication.Simulators.BackEndSim
 {
     class BackEndSim
     {
         public BackEndSim() { throw new NotImplementedException("TODO"); }
+        
+        public Collection<Appointment> RetrieveAppointments()
+        {
+            throw new NotImplementedException("TODO");
+        }
+
+        public void SendAppointmentSchedule(Schedule schedule)
+        {
+            throw new NotImplementedException("TODO");
+        }
+
+        public void AppointmentOverride(DateTime dateTime)
+        {
+            throw new NotImplementedException("TODO");
+        }
+
+        public void RadioTelescopeStatus(RadioTelescopeStatusEnum status)
+        {
+            throw new NotImplementedException("TODO");
+        }
+
+        public void SendRFData(RFData data)
+        {
+            throw new NotImplementedException("TODO");
+        }
+
+        public void SendCurrentOrientation(Orientation orientation)
+        {
+            throw new NotImplementedException("TODO");
+        }
+        
+        public void RecieveCommand(Command command)
+        {
+            throw new NotImplementedException("TODO");
+        }
     }
 }
