5a64377e9d319677b533b46a8ebe31d932f4d426
Jason Porter <jporter7@ycp.edu>
2018-11-13 00:37:29 -0500

    Moving methods from control room entity to control room controller.

---
+++ ControlRoomApplication/ControlRoomApplication/Controllers/ControlRoomController.cs
@@ -0,0 +1,54 @@
+﻿using ControlRoomApplication.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlRoomApplication.Controllers
+{
+    public class ControlRoomController
+    {
+        public ControlRoomController(ControlRoom controlRoom)
+        {
+            CRoom = controlRoom;
+        }
+
+        public void StartAppointment()
+        {
+            Appointment app = WaitingForNextAppointment();
+            Coordinate coordinate = CRoom.Context.Coordinates.Find(app.CoordinateId);
+            Orientation orientation = new Orientation();
+
+            // This stuff should actually be some conversion between right ascension/decl & az/el
+            orientation.Azimuth = coordinate.RightAscension;
+            orientation.Elevation = coordinate.Declination;
+            // but that can wait until later on.
+
+            CRoom.RadioTelescope.MoveRadioTelescope(orientation);
+        }
+
+        public Appointment WaitingForNextAppointment()
+        {
+            TimeSpan diff = new TimeSpan();
+            Appointment app = GetNextAppointment();
+            diff = app.StartTime - DateTime.Now;
+
+            while (diff.TotalMinutes > 5)
+            {
+                diff = app.StartTime - DateTime.Now;
+            }
+
+            return app;
+        }
+
+        public Appointment GetNextAppointment()
+        {
+            CRoom.Appointments.Sort((x, y) => DateTime.Compare(y.StartTime, x.StartTime));
+
+            return CRoom.Appointments[0];
+        }
+
+        public ControlRoom CRoom { get; set; }
+    }
+}
--- ControlRoomApplication/ControlRoomApplication/Entities/ControlRoom/ControlRoom.cs
+++ ControlRoomApplication/ControlRoomApplication/Entities/ControlRoom/ControlRoom.cs
@@ -20,41 +20,6 @@ namespace ControlRoomApplication.Entities
 
         }
 
-        public void StartAppointment()
-        {
-            Appointment app = WaitingForNextAppointment();
-            Coordinate coordinate = Context.Coordinates.Find(app.CoordinateId);
-            Orientation orientation = new Orientation();
-            orientation.Azimuth = coordinate.RightAscension;
-            orientation.Elevation = coordinate.Declination;
-
-            RadioTelescope.MoveRadioTelescope(orientation);
-        }
-
-        public Appointment WaitingForNextAppointment()
-        {
-            TimeSpan diff = new TimeSpan();
-            Appointment app = GetNextAppointment();
-            diff = app.StartTime - DateTime.Now;
-
-            while(diff.TotalMinutes > 5)
-            {
-                diff = app.StartTime - DateTime.Now;
-            }
-
-            return app;
-        }
-
-        public Appointment GetNextAppointment()
-        {
-            Appointments = Context.Appointments;
-            List<Appointment> apps = DbSetToList(Appointments);
-
-            apps.Sort((x, y) => DateTime.Compare(y.StartTime, x.StartTime));
-
-            return apps[0];
-        }
-
         private List<Appointment> DbSetToList(DbSet<Appointment> appointments)
         {
             List<Appointment> apps = new List<Appointment>();
@@ -69,7 +34,24 @@ namespace ControlRoomApplication.Entities
 
         public AbstractRadioTelescope RadioTelescope { get; set; }
         public AbstractSpectraCyber SpectraCyber { get; set; }
-        public DbSet<Appointment> Appointments { get; set; }
+        public List<Appointment> Appointments
+        {
+            get
+            {
+                List<Appointment> apps = new List<Appointment>();
+
+                foreach (Appointment app in Context.Appointments)
+                {
+                    apps.Add(app);
+                }
+
+                return apps;
+            }
+            set
+            {
+                Appointments = value;
+            }
+        }
         public RTDbContext Context { get; set; }
     }
 }
