366dd9566baae00de439c3ff6198410d51418481
Cody Spath <cspath1@ycp.edu>
2018-11-26 21:42:45 -0500

    Create OrientationCalculationController and build out basic test case

--- ControlRoomApplication/ControlRoomApplication/Constants/RadioTelescopeConstants.cs
+++ ControlRoomApplication/ControlRoomApplication/Constants/RadioTelescopeConstants.cs
@@ -1,7 +1,11 @@
-﻿namespace ControlRoomApplication.Constants
+﻿using AASharp;
+
+namespace ControlRoomApplication.Constants
 {
     public sealed class RadioTelescopeConstants
     {
-
+        public static readonly double OBSERVATORY_LONGITUDE = AASCoordinateTransformation.DMSToDegrees(40, 1, 27.872);
+        public static readonly double OBSERVATORY_LATITUDE = AASCoordinateTransformation.DMSToDegrees(76, 42, 16.430, false);
+        public static readonly double OBSERVATORY_HEIGHT = 395.0;
     }
 }
