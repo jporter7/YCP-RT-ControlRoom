4ef593c9416332c8dc5c833f8692f50c5d77bc72
GarrettRuths <gruths@ycp.edu>
2018-12-12 10:19:37 -0500

    Added priliminary Orientation Calculator

--- ControlRoomApplication/ControlRoomApplication/Controllers/AASharpControllers/CoordinateCalculationController.cs
+++ ControlRoomApplication/ControlRoomApplication/Controllers/AASharpControllers/CoordinateCalculationController.cs
@@ -25,5 +25,26 @@ namespace ControlRoomApplication.Controllers.AASharpControllers
                     return new Coordinate(0.0,0.0);
             }
         }
+
+        public Orientation CalculateOrientation(DateTime datetime, Coordinate coordinate)
+        {
+            double lat = 39.96;
+            double JD = AASharp.AASDate.DateToJD(datetime.Year,datetime.Month, datetime.Day,true);
+            double ST = AASharp.AASSidereal.ApparentGreenwichSiderealTime(JD);
+            //Greenwich hour angle
+            double GHA = ST - coordinate.RightAscension;
+            //use for caluclation local hour angle;
+            double LHADelta = 76.72 / 15;
+            double LHA = GHA - LHADelta;
+
+            //Calculate Azimuth and Elevation
+            // Reference - aa.usno.navy.mil/faq/docs/Alt_Az.php
+            Orientation calc = new Orientation();
+            calc.Azimuth = Math.Cos(LHA)*Math.Cos(lat)+Math.Sin(coordinate.Declination)*Math.Sin(lat);
+            calc.Elevation = (0-Math.Sin(LHA))/(Math.Tan(coordinate.Declination)*Math.Cos(lat) - Math.Sin(lat)*Math.Cos(LHA));
+            
+            return calc;
+        }
+
     }
 }
