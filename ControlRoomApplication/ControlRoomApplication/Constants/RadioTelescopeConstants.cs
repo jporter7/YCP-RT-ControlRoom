83b3d9d62760686a871c9988c89e086a1bcd6176
Chris Garrety <cgarret3@ycp.edu>
2018-12-13 09:39:48 -0500

    CoordinateTransformations
    Sun and Moon work with some innaccuracy (Sun is pretty good), working on CoordinateToOrientation

--- ControlRoomApplication/ControlRoomApplication/Constants/RadioTelescopeConstants.cs
+++ ControlRoomApplication/ControlRoomApplication/Constants/RadioTelescopeConstants.cs
@@ -2,6 +2,8 @@
 {
     public sealed class RadioTelescopeConstants
     {
-
+        public const double RT_LAT = 39.5012;
+        public const double RT_LONG = -76.0244;
+        public const double RT_ALT = 100;
     }
 }
