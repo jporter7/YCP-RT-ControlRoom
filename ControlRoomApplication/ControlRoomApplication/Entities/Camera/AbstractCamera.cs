c6c2365b1540e102092b14875529b3d93c2ec708
Chris Garrety <cgarret3@ycp.edu>
2019-02-08 13:20:32 -0500

    Update AbstractCamera.cs
    Constructor and CameraId

--- ControlRoomApplication/ControlRoomApplication/Entities/Camera/AbstractCamera.cs
+++ ControlRoomApplication/ControlRoomApplication/Entities/Camera/AbstractCamera.cs
@@ -1,12 +1,14 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ControlRoomApplication.Entities.Camera
 {
     class AbstractCamera
     {
+        public AbstractCamera()
+        {
+
+        }
+
+        int CameraId { get; set; }
     }
 }
