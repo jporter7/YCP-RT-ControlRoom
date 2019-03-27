715d5d07407f4a2776e8911799b14bb558213b44
Jason Porter <jporter7@ycp.edu>
2019-03-26 23:07:51 -0400

    Creating HeartbeatInterface test with a concrete test class and creating SpectraCyberTestController test.

---
+++ ControlRoomApplication/ControlRoomApplicationTest/EntitiesTests/HeartbeatInterfaceTests/ConcreteHeartbeatTestClass.cs
@@ -0,0 +1,21 @@
+﻿using ControlRoomApplication.Entities;
+
+namespace ControlRoomApplicationTest.EntitiesTests.HeartbeatInterfaceTests
+{
+    public class ConcreteHeartbeatTestClass : HeartbeatInterface
+    {
+        public ConcreteHeartbeatTestClass()
+        {
+        }
+
+        protected override bool KillHeartbeatComponent()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        protected override bool TestIfComponentIsAlive()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
---
+++ ControlRoomApplication/ControlRoomApplicationTest/EntityControllersTests/SpectraCyberTestControllerTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using ControlRoomApplication.Controllers.SpectraCyberController;
+using ControlRoomApplication.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ControlRoomApplicationTest.EntityControllersTests
+{
+    [TestClass]
+    public class SpectraCyberTestControllerTest
+    {
+        private SpectraCyberTestController spectraCyberTestController;
+        private SpectraCyberSimulator spectraCyberSimulator;
+
+        [TestInitialize]
+        public void BuildUp()
+        {
+            spectraCyberSimulator = new SpectraCyberSimulator();
+            spectraCyberTestController = new SpectraCyberTestController(spectraCyberSimulator);
+        }
+
+        [TestCleanup]
+        public void TearDown()
+        {
+
+        }
+
+        [TestCleanup]
+        public void TestInitialization()
+        {
+            Assert.IsNotNull(spectraCyberTestController);
+        }
+
+        [TestMethod]
+        public void TestBringUp()
+        {
+            Assert.IsTrue(spectraCyberTestController.BringUp());
+        }
+
+        [TestMethod]
+        public void TestBringDown()
+        {
+            Assert.IsTrue(spectraCyberTestController.BringDown());
+        }
+
+        [TestMethod]
+        public void TestIfComponentIsAlive()
+        {
+
+        }
+    }
+}
