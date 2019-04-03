4c1129b80a4ae27f03d84fb0f5a0d3d8e498bf8d
Daniel Reightneour <dreightn@ycp.edu>
2019-04-03 18:43:10 -0400

    Initial Commit for VRPLC Adds VRPLCDriver and VRPLCDriverTest

---
+++ ControlRoomApplication/ControlRoomApplication/Controllers/PLCCommunication/PLCDrivers/VRPLCDriver.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using ControlRoomApplication.Constants;
+using ControlRoomApplication.Entities;
+using System.IO.Pipes;
+namespace ControlRoomApplication.Controllers.PLCCommunication
+{
+	public class VRPLCDriver : AbstractPLCDriver
+	{
+
+		public VRPLCDriver(IPAddress ip_address, int port) : base(ip_address, port)
+		{
+		
+		}
+
+		public VRPLCDriver(string ip, int port) : this(IPAddress.Parse(ip), port) { }
+
+		protected override bool ProcessRequest(NetworkStream ActiveClientStream, byte[] query)
+		{
+			Console.WriteLine("We made it here at least");
+			byte[] FinalResponseContainer;
+			//Keeping this here for error handling
+			int ExpectedSize = query[0] + (16 * query[1]);
+			if (query.Length != ExpectedSize)
+			{
+				throw new ArgumentException(
+					"VRPLCDriverController read a package specifying a size [" + ExpectedSize.ToString() + "], but the actual size was different [" + query.Length + "]."
+				);
+			}
+			byte CommandQueryTypeAndExpectedResponseStatus = query[2];
+			byte CommandQueryTypeByte = (byte)(CommandQueryTypeAndExpectedResponseStatus & 0x3F);
+			byte ExpectedResponseStatusByte = (byte)(CommandQueryTypeAndExpectedResponseStatus >> 6);
+
+			PLCCommandAndQueryTypeEnum CommandQueryTypeEnum = PLCCommandAndQueryTypeConversionHelper.GetFromByte(CommandQueryTypeByte);
+			PLCCommandResponseExpectationEnum ExpectedResponseStatusEnum = PLCCommandResponseExpectationConversionHelper.GetFromByte(ExpectedResponseStatusByte);
+
+			//Passing the byte[] query to the Unity Engine
+			NamedPipeServerStream namedPipeServer = new NamedPipeServerStream("VR-pipe");
+			namedPipeServer.WaitForConnection();
+			//The lenght of the query is currently 19 (the base sive of our packets)
+			//not sure if this value will have to change in the future or not
+			//so leaving at 19 for now
+			namedPipeServer.Write(query, 0, 19);
+			namedPipeServer.WaitForPipeDrain();
+			//Now we get the response from the VR telescope
+			if (ExpectedResponseStatusEnum == PLCCommandResponseExpectationEnum.FULL_RESPONSE)
+			{
+				FinalResponseContainer = new byte[19];
+				Console.WriteLine("We made it here at least");
+				namedPipeServer.Read(FinalResponseContainer, 0, 19);
+				for(int i = 0; i < 19; i++)
+				{
+					Console.Write(FinalResponseContainer[i]+", ");
+				}
+				Console.Write("\n");
+			} else if(ExpectedResponseStatusEnum == PLCCommandResponseExpectationEnum.MINOR_RESPONSE)
+			{
+				FinalResponseContainer = new byte[3];
+				namedPipeServer.Read(FinalResponseContainer, 0, 3);
+			}
+			else
+			{
+				throw new ArgumentException("Invalid PLCCommandResponseExpectationEnum value seen while processing client request in ScaleModelPLCDriver: " + ExpectedResponseStatusEnum.ToString());
+			}
+				
+
+			//Were dont communicating for now, so were disconnecting and distroying the named pipeline
+			namedPipeServer.Disconnect();
+			namedPipeServer.Close();
+
+			return AttemptToWriteDataToServer(ActiveClientStream, FinalResponseContainer);
+		}
+	}
+}
---
+++ ControlRoomApplication/ControlRoomApplicationTest/EntityControllersTests/VRPLCDriverTest.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ControlRoomApplication.Constants;
+using ControlRoomApplication.Controllers.PLCCommunication;
+using ControlRoomApplication.Controllers;
+using ControlRoomApplication.Entities;
+
+namespace ControlRoomApplicationTest.EntityControllersTests
+{
+	[TestClass]
+	public class VRPLCDriverTest
+	{
+		public static PLCClientCommunicationHandler CommsHandler;
+		public static VRPLCDriver TestDriver;
+
+		[ClassInitialize]
+		public static void BuildUp(TestContext testContext)
+		{
+			string ip = PLCConstants.LOCAL_HOST_IP;
+			int port = PLCConstants.PORT_8080;
+
+			CommsHandler = new PLCClientCommunicationHandler(ip, port);
+			TestDriver = new VRPLCDriver(ip, port);
+
+			TestDriver.StartAsyncAcceptingClients();
+
+			CommsHandler.ConnectToServer();
+		}
+
+		[TestMethod]
+		public void TestConnectionTest()
+		{
+			byte[] expectedResponse = new byte[] { 0x13, 0x0, 0x1, 0x1, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0, 0x0 };
+			byte[] actualResponse = CommsHandler.RequestMessageSend(PLCCommandAndQueryTypeEnum.TEST_CONNECTION);
+
+			Console.WriteLine("Test Connection (actual): [{0}]", string.Join(", ", actualResponse));
+			Console.WriteLine("Test Connection (expected): [{0}]", string.Join(", ", expectedResponse));
+
+			CollectionAssert.AreEqual(expectedResponse, actualResponse);
+		}
+
+		[TestMethod]
+		public void TestSendAzeEle()
+		{
+			double NewAzimuth = 90;
+			double NewElevation = 45;
+
+			byte[] expectedResponse = new byte[] { 0x3, 0x0, 0x1 };
+			byte[] actualResponse = CommsHandler.RequestMessageSend(PLCCommandAndQueryTypeEnum.SET_OBJECTIVE_AZEL_POSITION, new Orientation(NewAzimuth, NewElevation));
+
+			Console.WriteLine("Test Set Active Objective AZ/EL (actual): [{0}]", string.Join(", ", actualResponse));
+			Console.WriteLine("Test Set Active Objective AZ/EL (expected): [{0}]", string.Join(", ", expectedResponse));
+
+			CollectionAssert.AreEqual(expectedResponse, actualResponse);
+			
+		}
+		/*
+		[TestMethod]
+		public void TestDemo()
+		{
+			sendcoords(90, 45);
+			sleep(17);
+			sendcoords(0,0);
+			sleep(17);
+			sendcoords(45, 45);
+			sleep(15);
+			sendcoords(90, 45);
+			sleep(7);
+			sendcoords(90, 55);
+			sleep(4);
+			sendcoords(45, 55);
+			sleep(7);
+			sendcoords(45, 60);
+			sleep(4);
+			sendcoords(90, 60);
+			sleep(7);
+			sendcoords(90, 70);
+			sleep(4);
+			sendcoords(45, 70);
+			sleep(7);
+			sendcoords(0, 90);
+		}
+		
+
+		private void sendcoords(Double NewAzimuth, Double NewElevation)
+		{
+			CommsHandler.RequestMessageSend(PLCCommandAndQueryTypeEnum.SET_OBJECTIVE_AZEL_POSITION, new Orientation(NewAzimuth, NewElevation));
+		}
+
+		private void sleep(double seconds)
+		{
+
+			System.Threading.Thread.Sleep((int)seconds * 1000);
+		}*/
+	}
+}
