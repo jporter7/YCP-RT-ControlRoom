89c9cee64805367c7afa6d3c6efa8bd1fb729f4e
Chris Garrety <cgarret3@ycp.edu>
2019-04-04 06:23:12 -0400

    Finished SpectraCyberSimulatorControllerTest

--- ControlRoomApplication/ControlRoomApplicationTest/EntityControllersTests/SpectraCyberSimulatorControllerTest.cs
+++ ControlRoomApplication/ControlRoomApplicationTest/EntityControllersTests/SpectraCyberSimulatorControllerTest.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ControlRoomApplication.Controllers;
+using ControlRoomApplication.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -10,12 +12,28 @@ namespace ControlRoomApplicationTest.EntityControllersTests
     [TestClass]
     public class SpectraCyberSimulatorControllerTest
     {
+        private SpectraCyberSimulatorController _spectraCyberController;
+        private SpectraCyberSimulator _spectraCyberSimulator;
+
         [TestInitialize]
         public void BuildUp()
         {
-
+            _spectraCyberSimulator = new SpectraCyberSimulator();
+            _spectraCyberController = new SpectraCyberSimulatorController(_spectraCyberSimulator);
         }
 
-        //[TestMethod]
+        [TestMethod]
+        public void TestConstructor()
+        {
+            Assert.IsNotNull(_spectraCyberController);
+        }
+
+        [TestMethod]
+        public void TestBringUpBringDown()
+        {
+            Assert.IsTrue(_spectraCyberController.BringUp());
+            Assert.IsTrue(_spectraCyberController.BringDown());
+        }
+
     }
 }
