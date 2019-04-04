88e544439898c75182931b532ecead23ab844b88
Chris Garrety <cgarret3@ycp.edu>
2019-04-04 06:27:23 -0400

    Cleaned up unused directives
    PLCCommandAndQueryTypeEnumTest
    SpectraCyberIntegrationTimeEnumTest
    SpectraCyberSimulatorControllerTest

--- ControlRoomApplication/ControlRoomApplicationTest/EntityControllersTests/SpectraCyberSimulatorControllerTest.cs
+++ ControlRoomApplication/ControlRoomApplicationTest/EntityControllersTests/SpectraCyberSimulatorControllerTest.cs
@@ -1,11 +1,6 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using ControlRoomApplication.Controllers;
 using ControlRoomApplication.Entities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ControlRoomApplicationTest.EntityControllersTests
 {
