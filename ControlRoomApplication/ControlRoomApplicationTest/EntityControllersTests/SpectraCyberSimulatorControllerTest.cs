8ad9fa6535d3b20291714fed8e27f872032a8dd3
Chris Garrety <cgarret3@ycp.edu>
2019-04-04 05:51:37 -0400

    Create SpectraCyberSimulatorControllerTest

---
+++ ControlRoomApplication/ControlRoomApplicationTest/EntityControllersTests/SpectraCyberSimulatorControllerTest.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlRoomApplicationTest.EntityControllersTests
+{
+    [TestClass]
+    public class SpectraCyberSimulatorControllerTest
+    {
+        [TestInitialize]
+        public void BuildUp()
+        {
+
+        }
+
+        //[TestMethod]
+    }
+}
