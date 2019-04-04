450b37471053b0cbe7611b17af0d8f8f5f3ccead
Chris Garrety <cgarret3@ycp.edu>
2019-04-04 05:47:47 -0400

    PLCCommandAndQueryTest Update
    finished

--- ControlRoomApplication/ControlRoomApplicationTest/EntitiesTests/PLCCommandAndQueryTypeEnumTest.cs
+++ ControlRoomApplication/ControlRoomApplicationTest/EntitiesTests/PLCCommandAndQueryTypeEnumTest.cs
@@ -9,7 +9,7 @@ using System.Threading.Tasks;
 namespace ControlRoomApplicationTest.EntitiesTests
 {
     [TestClass]   
-    class PLCCommandAndQueryTypeEnumTest
+    public class PLCCommandAndQueryTypeEnumTest
     {
         private PLCCommandAndQueryTypeEnum _input_undefined;
         private PLCCommandAndQueryTypeEnum _input_test_connection;
@@ -38,8 +38,32 @@ namespace ControlRoomApplicationTest.EntitiesTests
         [TestMethod]
         public void TestPLCCommandAndQueryTypeEnumHelper()
         {
-            byte output = PLCCommandAndQueryTypeConversionHelper.ConvertToByte(_input_undefined);
-            Assert.AreEqual(0x0, _input_undefined);
+            Assert.AreEqual(0x0, PLCCommandAndQueryTypeConversionHelper.ConvertToByte(_input_undefined));
+            Assert.AreEqual(PLCCommandAndQueryTypeEnum.UNDEFINED, PLCCommandAndQueryTypeConversionHelper.GetFromByte(0x0));
+
+            Assert.AreEqual(0x1, PLCCommandAndQueryTypeConversionHelper.ConvertToByte(_input_test_connection));
+            Assert.AreEqual(PLCCommandAndQueryTypeEnum.TEST_CONNECTION, PLCCommandAndQueryTypeConversionHelper.GetFromByte(0x1));
+
+            Assert.AreEqual(0x2, PLCCommandAndQueryTypeConversionHelper.ConvertToByte(_input_get_current_azel_positions));
+            Assert.AreEqual(PLCCommandAndQueryTypeEnum.GET_CURRENT_AZEL_POSITIONS, PLCCommandAndQueryTypeConversionHelper.GetFromByte(0x2));
+
+            Assert.AreEqual(0x3, PLCCommandAndQueryTypeConversionHelper.ConvertToByte(_input_get_current_limit_switch_statuses));
+            Assert.AreEqual(PLCCommandAndQueryTypeEnum.GET_CURRENT_LIMIT_SWITCH_STATUSES, PLCCommandAndQueryTypeConversionHelper.GetFromByte(0x3));
+
+            Assert.AreEqual(0x4, PLCCommandAndQueryTypeConversionHelper.ConvertToByte(_input_get_current_safety_interlock_status));
+            Assert.AreEqual(PLCCommandAndQueryTypeEnum.GET_CURRENT_SAFETY_INTERLOCK_STATUS, PLCCommandAndQueryTypeConversionHelper.GetFromByte(0x4));
+
+            Assert.AreEqual(0x5, PLCCommandAndQueryTypeConversionHelper.ConvertToByte(_input_cancel_active_objective_azel_position));
+            Assert.AreEqual(PLCCommandAndQueryTypeEnum.CANCEL_ACTIVE_OBJECTIVE_AZEL_POSITION, PLCCommandAndQueryTypeConversionHelper.GetFromByte(0x5));
+
+            Assert.AreEqual(0x6, PLCCommandAndQueryTypeConversionHelper.ConvertToByte(_input_shutdown));
+            Assert.AreEqual(PLCCommandAndQueryTypeEnum.SHUTDOWN, PLCCommandAndQueryTypeConversionHelper.GetFromByte(0x6));
+
+            Assert.AreEqual(0x7, PLCCommandAndQueryTypeConversionHelper.ConvertToByte(_input_calibrate));
+            Assert.AreEqual(PLCCommandAndQueryTypeEnum.CALIBRATE, PLCCommandAndQueryTypeConversionHelper.GetFromByte(0x7));
+
+            Assert.AreEqual(0x8, PLCCommandAndQueryTypeConversionHelper.ConvertToByte(_input_set_objective_azel_position));
+            Assert.AreEqual(PLCCommandAndQueryTypeEnum.SET_OBJECTIVE_AZEL_POSITION, PLCCommandAndQueryTypeConversionHelper.GetFromByte(0x8));
         }
     }
 }
