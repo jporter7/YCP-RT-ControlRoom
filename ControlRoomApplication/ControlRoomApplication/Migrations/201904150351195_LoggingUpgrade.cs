6544765089bc68e702aade852062d068d8bbd98b
Jason Porter <jporter7@ycp.edu>
2019-04-15 00:00:37 -0400

    Running database migration.

--- ControlRoomApplication/ControlRoomApplication/Migrations/201904150351195_LoggingUpgrade.cs
+++ ControlRoomApplication/ControlRoomApplication/Migrations/201904150351195_LoggingUpgrade.cs
@@ -7,10 +7,23 @@ namespace ControlRoomApplication.Migrations
     {
         public override void Up()
         {
+            CreateTable(
+                "dbo.telescope_log",
+                c => new
+                {
+                    Id = c.Int(nullable: false, identity: true),
+                    log_date = c.DateTime(nullable: false, precision: 0),
+                    thread = c.String(nullable: false, unicode: false),
+                    log_level = c.String(nullable: false, unicode: false),
+                    logger = c.String(nullable: false, unicode: false),
+                    message = c.String(nullable: false, unicode: false),
+                })
+                .PrimaryKey(t => t.Id);
         }
-        
+
         public override void Down()
         {
+            DropTable("dbo.telescope_log");
         }
     }
 }
