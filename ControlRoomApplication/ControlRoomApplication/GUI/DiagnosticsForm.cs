c964a18dcb1e29584598c9bc3d2ea71b2af1b32c
AktionP <eidson.kyle@gmail.com>
2019-11-03 21:20:44 -0500

    Slight update to temp
    After looking through and trying to find out where the variables in the other box were linked, I found nothing. So I have updated the temps so that we can eventually shift between celsius and fahrenheit

--- ControlRoomApplication/ControlRoomApplication/GUI/DiagnosticsForm.cs
+++ ControlRoomApplication/ControlRoomApplication/GUI/DiagnosticsForm.cs
@@ -39,10 +39,11 @@ namespace ControlRoomApplication.GUI
         private double[] elEncDemo = { 0, 0.2, 0.4, 0.6, 0.8, 1, 1.2, 1.4, 1.6, 1.8, 2.0, 2.2, 2.4, 2.6, 2.8, 3.0, 3.2, 3.4, 3.6, 3.8, 4.0, 4.2, 4.4, 4.6, 4.8, 5.0, 5.2, 5.4, 5.6, 5.8, 6.0, 6.2, 6.4, 6.6, 6.8, 7.0, 7.2, 7.4, 7.6, 7.8, 8.0, 8.2, 8.4, 8.6, 8.8, 9.0, 9.2, 9.4, 9.6, 9.8, 10.0, 10.2, 10.4, 10.6, 10.8, 11.0, 11.2, 11.4, 11.6, 11.8, 12.0, 12.2, 12.4, 12.6, 12.8, 13.0, 13.2, 13.4, 13.6, 13.8, 14.0, 14.2, 14.4, 14.6, 14.8, 15.0, 15.2, 15.4, 15.6, 15.8, 16.0 }; //10 bits of precision, 2.8 
 
         DateTime currentTempDate = DateTime.Now;
- 
+
         /***********DEMO MODE VARIABLES END*********/
-    
-       
+
+        bool celOrFar = false; // True is celsius, false is fahrenheit
+
         double _azEncoderDegrees = 0;
         double _elEncoderDegrees = 0;
         double _elevationTemp = 0;
@@ -314,7 +315,12 @@ namespace ControlRoomApplication.GUI
 
             }
             
-            
+            if(celOrFar)
+            {
+                elevationTemperature = (elevationTemperature - 32) * (5.0/9);
+                azimuthTemperature = (azimuthTemperature - 32) * (5.0 / 9);
+            }
+
             fldElTemp.Text = elevationTemperature.ToString();
             fldAzTemp.Text = azimuthTemperature.ToString();
 
