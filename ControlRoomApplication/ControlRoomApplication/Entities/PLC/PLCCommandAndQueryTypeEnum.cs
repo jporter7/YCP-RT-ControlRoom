f4680d16477a83907e674aa12f8857250475da9c
minor <jminor1@ycp.edu>
2020-02-03 16:58:56 -0500

    add overrides to plc register map

--- ControlRoomApplication/ControlRoomApplication/Entities/PLC/PLCCommandAndQueryTypeEnum.cs
+++ ControlRoomApplication/ControlRoomApplication/Entities/PLC/PLCCommandAndQueryTypeEnum.cs
@@ -60,22 +60,42 @@ namespace ControlRoomApplication.Entities
     public enum PLC_modbus_server_register_mapping : ushort
     {
         /// <summary>
+        /// <item><description><para><em>OUT</em></para></description></item>
+        /// the PLC will not honor any requests to override the limits,
+        /// this is only a formality and testing
+        /// </summary>
+        LIMIT_OVERRIDE = 0+1,
+        /// <summary>
+        /// <item><description><para><em>OUT</em></para></description></item>
+        /// when this is set the elescope will still move even with the gate open
+        /// </summary>
+        GATE_OVERRIDE = 1+1,
+        /// <summary>
+        /// <item><description><para><em>OUT</em></para></description></item>
+        /// the PLC will never honor this request under any circumstances,
+        /// this mainly serves as a place holder
+        /// </summary>
+        E_STOP_OVERRIDE = 2+1,
+
+
+        /// <summary>
+        /// <item><description><para><em>IN</em></para></description></item>
         /// limit at -10
         /// </summary>
-        AZ_0_LIMIT  = 8 + 1,
+        AZ_0_LIMIT = 8 + 1,
         /// <summary>
+        /// <item><description><para><em>IN</em></para></description></item>
         /// limit at 375 degrees
         /// </summary>
         AZ_375_LIMIT = 9 + 1,
-
-
-        
         /// <summary>
+        /// <item><description><para><em>IN</em></para></description></item>
         /// home sensor at 0 degrees,
         /// will be active between -15 and 0
         /// </summary>
         AZ_0_HOME = 10 + 1,
         /// <summary>
+        /// <item><description><para><em>IN</em></para></description></item>
         /// SECONDARY home sensor at 0 degrees,
         /// will be active between -1 and 15
         /// </summary>
@@ -83,6 +103,7 @@ namespace ControlRoomApplication.Entities
 
 
         /// <summary>
+        /// <item><description><para><em>IN</em></para></description></item>
         /// limit switch at -10
         /// </summary>
         /// <remarks>
@@ -90,41 +111,58 @@ namespace ControlRoomApplication.Entities
         /// </remarks>
         EL_10_LIMIT = 12 + 1,
         /// <summary>
+        /// <item><description><para><em>IN</em></para></description></item>
         /// active between 0 and -15 degrees
         /// </summary>
         EL_0_HOME = 13 + 1,
         /// <summary>
+        /// <item><description><para><em>IN</em></para></description></item>
         /// limit at 90 degrees
         /// </summary>
         EL_90_LIMIT = 14 + 1,
 
+
+
         /// <summary>
+        /// <item><description><para><em>IN</em></para></description></item>
         /// emergency stop the plc will handle disabling the motors and MCU, the controll should re-configure (and posibly re-home) the MCU after this is ended
         /// </summary>
         E_STOP = 15 + 1,
         /// <summary>
+        /// <item><description><para><em>IN</em></para></description></item>
         /// high when gate is open
         /// </summary>
         Safety_INTERLOCK = 16 + 1,
 
         /// <summary>
+        /// <item><description><para><em>IN</em></para></description></item>
         /// high when the MCU has a fault on the AZ axsis
         /// </summary>
-        AZ_MCU_FAULT =17+1,
+        AZ_MCU_FAULT = 17+1,
         /// <summary>
+        /// <item><description><para><em>IN</em></para></description></item>
         /// high when the MCU has a fault on the AZ axsis
         /// </summary>
         EL_MCU_FAULT = 18+1,
         /// <summary>
+        /// <item><description><para><em>IN</em></para></description></item>
         /// high when the azimuth motor has a fault
         /// </summary>
-        AZ_MTR_DRIVER_FAULT = 17 + 1,
+        AZ_MTR_DRIVER_FAULT = 19 + 1,
         /// <summary>
+        /// <item><description><para><em>IN</em></para></description></item>
         /// high when the ELEVATION motor has a fault
         /// </summary>
-        EL_MTR_DRIVER_FAULT = 18 + 1,
+        EL_MTR_DRIVER_FAULT = 20 + 1,
 
+
+        /// <summary>
+        /// not currerntly used by the PLC
+        /// </summary>
         AZ_MOTOR_CURRENT = 40 + 1,
+        /// <summary>
+        /// not currerntly used by the PLC
+        /// </summary>
         EL_MOTOR_CURRENT = 42 + 1
     }
 
