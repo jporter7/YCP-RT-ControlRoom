acc39d8859379ee7a439e7f24b1888db9de1c775
Kate Kennelly <kkennelly@ycp.edu>
2020-02-24 18:56:32 -0500

    Adding threading to script calls

--- ControlRoomApplication/ControlRoomApplication/GUI/RTControlForm.cs
+++ ControlRoomApplication/ControlRoomApplication/GUI/RTControlForm.cs
@@ -499,55 +499,87 @@ namespace ControlRoomApplication.Main
 
             RadioTelescope tele = rtController.RadioTelescope;
 
+            Thread thread;
+
             switch (caseSwitch)
             {
                 case 0:
-                    rtController.ExecuteRadioTelescopeControlledStop();
-                    tele.PLCDriver.Stow().GetAwaiter();
+                    thread = new Thread(() =>
+                    {
+                        rtController.ExecuteRadioTelescopeControlledStop();
+                        tele.PLCDriver.Stow().GetAwaiter();
+                    });
                     //Stow Script selected (index 0 of control script combo)
                     break;
                 case 1:
-                    rtController.ExecuteRadioTelescopeControlledStop();
-                    tele.PLCDriver.FullElevationMove().GetAwaiter();
+                    thread = new Thread(() =>
+                    {
+                        rtController.ExecuteRadioTelescopeControlledStop();
+                        tele.PLCDriver.FullElevationMove().GetAwaiter();
+                    });
                     //Full Elevation selected (index 1 of control script combo)
                     break;
                 case 2:
-                    rtController.ExecuteRadioTelescopeControlledStop();
-                    tele.PLCDriver.Full_360_CW_Rotation().GetAwaiter();
+                    thread = new Thread(() =>
+                    {
+                        rtController.ExecuteRadioTelescopeControlledStop();
+                        tele.PLCDriver.Full_360_CW_Rotation().GetAwaiter();
+                    });
                     //Full 360 CW selected (index 2 of control script combo)
                     break;
                 case 3:
-                    rtController.ExecuteRadioTelescopeControlledStop();
-                    tele.PLCDriver.Full_360_CCW_Rotation().GetAwaiter();
+                    thread = new Thread(() =>
+                    {
+                        rtController.ExecuteRadioTelescopeControlledStop();
+                        tele.PLCDriver.Full_360_CCW_Rotation().GetAwaiter();
+                    });
                     //Full 360 CCW  selected (index 3 of control script combo)
                     break;
                 case 4:
-                    rtController.ExecuteRadioTelescopeControlledStop();
-                    tele.PLCDriver.Thermal_Calibrate().GetAwaiter();
+                    thread = new Thread(() =>
+                    {
+                        rtController.ExecuteRadioTelescopeControlledStop();
+                        tele.PLCDriver.Thermal_Calibrate().GetAwaiter();
+                    });
                     //Thermal Calibration selected (index 4 of control script combo)
                     break;
                 case 5:
-                    rtController.ExecuteRadioTelescopeControlledStop();
-                    tele.PLCDriver.SnowDump().GetAwaiter();
+                    thread = new Thread(() =>
+                    {
+                        rtController.ExecuteRadioTelescopeControlledStop();
+                        tele.PLCDriver.SnowDump().GetAwaiter();
+                    });
                     //Snow Dump selected (index 5 of control script combo)
                     break;
                 case 6:
-                    rtController.ExecuteRadioTelescopeControlledStop();
-                    tele.PLCDriver.RecoverFromLimitSwitch().GetAwaiter();
+                    thread = new Thread(() =>
+                    {
+                        rtController.ExecuteRadioTelescopeControlledStop();
+                        tele.PLCDriver.RecoverFromLimitSwitch().GetAwaiter();
+                    });
                     //Recover from Limit Switch (index 6 of control script combo)
                     break;
                 case 7:
-                    rtController.ExecuteRadioTelescopeControlledStop();
-                    tele.PLCDriver.Recover_CW_Hardstop().GetAwaiter();
+                    thread = new Thread(() =>
+                    {
+                        rtController.ExecuteRadioTelescopeControlledStop();
+                        tele.PLCDriver.Recover_CW_Hardstop().GetAwaiter();
+                    });
                     //Recover from Clockwise Hardstop (index 7 of control script combo)
                     break;
                 case 8:
-                    rtController.ExecuteRadioTelescopeControlledStop();
-                    tele.PLCDriver.Recover_CCW_Hardstop().GetAwaiter();
+                    thread = new Thread(() =>
+                    {
+                        rtController.ExecuteRadioTelescopeControlledStop();
+                        tele.PLCDriver.Recover_CCW_Hardstop().GetAwaiter();
+                    });
                     //Recover from Counter-Clockwise Hardstop (index 8 of control script combo)
                     break;
                 case 9:
-                    controlRoom.RadioTelescopes[rtId-1].PLCDriver.Home();
+                    thread = new Thread(() =>
+                    {
+                        tele.PLCDriver.Home();
+                    });
                     //Recover from Counter-Clockwise Hardstop (index 9 of control script combo)
                     break;
                 default:
