fcfa59462c4f187cb68622c3bae7f287af092d4d
David Nelson <supermariokart98@gmail.com>
2020-10-30 18:50:49 -0400

    Updated comments
    Issue #167

--- ControlRoomApplication/ControlRoomApplication/Controllers/Communications/Encryption/AES.cs
+++ ControlRoomApplication/ControlRoomApplication/Controllers/Communications/Encryption/AES.cs
@@ -12,11 +12,13 @@ namespace ControlRoomApplication.Controllers.Communications.Encryption
     {
         public static byte[] Encrypt(string plainText)
         {
+            // Retrieve keys, located in the same directory as the Control Room executable
             byte[] Key = File.ReadAllBytes("AESKey.bin");
             byte[] IV = File.ReadAllBytes("IV.bin");
 
             byte[] encrypted;
 
+            // Prepend a timestamp and then interlace the text
             plainText = Lace(generateTimestamp(plainText));
 
             // Create a new AesManaged.    
@@ -24,15 +26,12 @@ namespace ControlRoomApplication.Controllers.Communications.Encryption
             {
                 // Create encryptor    
                 ICryptoTransform encryptor = aes.CreateEncryptor(Key, IV);
-                // Create MemoryStream    
+
                 using (MemoryStream ms = new MemoryStream())
-                {
-                    // Create crypto stream using the CryptoStream class. This class is the key to encryption    
-                    // and encrypts and decrypts data from any given stream. In this case, we will pass a memory stream    
-                    // to encrypt    
+                {    
                     using (CryptoStream cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
                     {
-                        // Create StreamWriter and write data to a stream    
+                        // Create StreamWriter and write memory stream to a byte array    
                         using (StreamWriter sw = new StreamWriter(cs))
                             sw.Write(plainText);
                         encrypted = ms.ToArray();
@@ -45,6 +44,7 @@ namespace ControlRoomApplication.Controllers.Communications.Encryption
         }
         public static string Decrypt(byte[] cipherText)
         {
+            // Retrieve keys, located in the same directory as the Control Room executable
             byte[] Key = File.ReadAllBytes("AESKey.bin");
             byte[] IV = File.ReadAllBytes("IV.bin");
             string plaintext = null;
@@ -54,18 +54,18 @@ namespace ControlRoomApplication.Controllers.Communications.Encryption
             {
                 // Create a decryptor    
                 ICryptoTransform decryptor = aes.CreateDecryptor(Key, IV);
-                // Create the streams used for decryption.    
                 using (MemoryStream ms = new MemoryStream(cipherText))
                 {
-                    // Create crypto stream    
                     using (CryptoStream cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
                     {
-                        // Read crypto stream    
+                        // convert stream to String
                         using (StreamReader reader = new StreamReader(cs))
                             plaintext = reader.ReadToEnd();
                     }
                 }
             }
+
+            // Unlace the text, then verify/remove the timestamp
             return verifyAndRemoveTimestamp(UnLace(plaintext));
         }
 
