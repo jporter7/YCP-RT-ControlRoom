e9583d81edf37bff137225c5ebb681cebb60c6f8
Patrick <pnelson1@ycp.edu>
2021-03-20 02:59:20 -0400

    Changed default telescope type to SLIP_RING
    At long last...we are finally in a good place to make this change.
    
    Issue #277

--- ControlRoomApplication/ControlRoomApplication/GUI/MainForm.cs
+++ ControlRoomApplication/ControlRoomApplication/GUI/MainForm.cs
@@ -467,8 +467,8 @@ namespace ControlRoomApplication.Main
                 newRT.CurrentOrientation = new Entities.Orientation(0, 0);
 
                 // This is the TELESCOPE TYPE
-                // It is only set to HARD_STOPS right now because that is what we are using for testing
-                newRT._TeleType = RadioTelescopeTypeEnum.HARD_STOPS;
+                // It is now set to SLIP_RING because we finally removed the hard stops. Isn't that exciting?!
+                newRT._TeleType = RadioTelescopeTypeEnum.SLIP_RING;
 
                 DatabaseOperations.AddRadioTelescope(newRT);
 
