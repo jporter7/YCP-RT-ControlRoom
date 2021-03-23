5bf5d200b8fe2d2184c97f57b27bb352a9d9bc9c
davidmchugh2000 <dmchugh@ycp.edu>
2021-03-22 23:13:41 -0400

    Impliment IEquatable in Temperature
    Added IEEquatable and added Juinit testing
    
    Issue #265

---
+++ ControlRoomApplication/ControlRoomApplicationTest/EntitiesTests/TemperatureTest.cs
@@ -0,0 +1,42 @@
+﻿using ControlRoomApplication.Entities;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ControlRoomApplicationTest.EntitiesTests
+{
+    [TestClass]
+    public class TemperatureTest
+    {
+        // Class being tested
+        private Temperature Temperature;
+        private int longitude = 100;
+        private int latitude = 34;
+        private int altitude = 40;
+
+        [TestInitialize]
+        public void BuildUp()
+        {
+
+        }
+
+        [TestMethod]
+        public void TestGettersAndSetters()
+        {
+            // Initialize appointment entity
+            SensorLocationEnum loc1 = SensorLocationEnum.AZ_MOTOR;
+
+            //Generate current time
+            long dateTime = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+
+            //Generate Temperature
+            Temperature t1 = Temperature.Generate(dateTime, 0.0, loc1);
+            Temperature t2 = Temperature.Generate(dateTime, 2.0, loc1);
+
+
+            Assert.AreEqual(t1, t1);
+            Assert.AreNotEqual(t1, t2);
+            Assert.AreNotEqual(t2, t1);
+        }
+    }
+}
+    
