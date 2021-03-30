c7bb8d7995972fc63b86326dec40ebd885c54680
Patrick <pnelson1@ycp.edu>
2021-03-30 14:53:21 -0400

    Updated Sensor Overrides tab to also include Init
    Issue #248

--- ControlRoomApplication/ControlRoomApplication/Controllers/PLCCommunication/PLCDrivers/ProductionPLCDriver.cs
+++ ControlRoomApplication/ControlRoomApplication/Controllers/PLCCommunication/PLCDrivers/ProductionPLCDriver.cs
@@ -557,8 +557,8 @@ namespace ControlRoomApplication.Controllers
         /// <summary>
         /// This is a script that is called when we want to check the current thermal calibration of the telescope
         /// Moves to point to the tree, reads in data, gets data from weather station, and compares
-        /// Postcondition: return true if the telescope data IS within 0.001 degrees Farenheit
-        ///                return false if the telescope data IS NOT within 0.001 degrees Farenheit
+        /// Postcondition: return true if the telescope data IS within 0.001 degrees fahrenheit
+        ///                return false if the telescope data IS NOT within 0.001 degrees fahrenheit
         /// </summary>
         public override Task<bool> Thermal_Calibrate() {
             Orientation current = read_Position();
@@ -590,7 +590,7 @@ namespace ControlRoomApplication.Controllers
             double speedConstant = 299792458;
             double temperature = (rfResponse.Intensity * time * weinConstant) / (planckConstant * speedConstant);
 
-            // convert to farenheit
+            // convert to fahrenheit
             temperature = temperature * (9 / 5) - 459.67;
 
             // check against weather station reading
