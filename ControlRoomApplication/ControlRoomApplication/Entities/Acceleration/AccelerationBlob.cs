a1f2e716dc8e1bb94d3535728aea55ba5fb91d9e
Jack Anderson <60368246+andersonjackc@users.noreply.github.com>
2021-10-05 19:08:36 -0400

    Update how Acceleration Data is Blobbed together as well as parsed
    Removes acceleration magnitude from every acceleration point, adds time captured to the first sample in the sub blob.  Small bit of inaccuracy brought about by the (long) cast.

--- ControlRoomApplication/ControlRoomApplication/Entities/Acceleration/AccelerationBlob.cs
+++ ControlRoomApplication/ControlRoomApplication/Entities/Acceleration/AccelerationBlob.cs
@@ -2,6 +2,7 @@
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Text;
+using ControlRoomApplication.Constants;
 
 namespace ControlRoomApplication.Entities
 {
@@ -42,21 +43,35 @@ namespace ControlRoomApplication.Entities
             int length = x.Length - 1;
 
             //IF the next Acceleration array will bring us over 4000
-            if (BlobStringCounter + x.Length > 4000)
+            if (BlobStringCounter + x.Length >= MiscellaneousConstants.BLOB_SIZE)
             {
                 //Loop through array of acceleration object EXCEPT the last object 
                 for (int i = 0; i < x.Length - 1; i++)
                 {
-                    //Objects appended to StringBuilder with a $ added to sperate each value 
-                    //~ breaks up a single acceleration 
-                    //$ breaks up acceleratrion objects
-                    BlobTheBuilder.Append(x[i].acc + "~" + x[i].x + "~"
-                        + x[i].y + "~" + x[i].z + "~" + x[i].location_ID + "$");
+                    
+                    //if this is the first of the Accelerometer's returned datapoints, add the time captured to the blob
+                    if((BlobStringCounter+i)% MiscellaneousConstants.SUB_BLOB_SIZE == 0)
+                    {
+                        //Objects appended to StringBuilder with a $ added to sperate each value 
+                        //~ breaks up a single acceleration 
+                        //$ breaks up acceleratrion objects
+                        BlobTheBuilder.Append(x[i].TimeCaptured + "~" + x[i].x + "~"
+                            + x[i].y + "~" + x[i].z + "~" + x[i].location_ID + "$");
+                    }
+                    else
+                    {
+                        //Objects appended to StringBuilder with a $ added to sperate each value 
+                        //~ breaks up a single acceleration 
+                        //$ breaks up acceleratrion objects
+                        BlobTheBuilder.Append(x[i].x + "~" + x[i].y + "~" + x[i].z + "~" + x[i].location_ID + "$");
+                    }
+                    
+                    
+                    
                 }
 
                 //Last acceleration added without seperation
-                BlobTheBuilder.Append(x[length].acc + "~" + x[length].x + "~"
-                        + x[length].y + "~" + x[length].z + "~" + x[length].location_ID);
+                BlobTheBuilder.Append(x[length].x + "~" + x[length].y + "~" + x[length].z + "~" + x[length].location_ID);
             }
             //Under 4000 and needs a $ at the end
             else
@@ -64,17 +79,29 @@ namespace ControlRoomApplication.Entities
                 //Loop through array of acceleration object
                 for (int i = 0; i < x.Length; i++)
                 {
-                    //Objects appended to StringBuilder with a $ added to sperate each value 
-                    //~ breaks up a single acceleration 
-                    //$ breaks up acceleratrion objects
-                    BlobTheBuilder.Append(x[i].acc + "~" + x[i].x + "~"
-                        + x[i].y + "~" + x[i].z + "~" + x[i].location_ID + "$");
+
+                    //if this is the first of the Accelerometer's returned datapoints, add the time captured to the blob
+                    if ((BlobStringCounter + i) % MiscellaneousConstants.SUB_BLOB_SIZE == 0)
+                    {
+                        //Objects appended to StringBuilder with a $ added to sperate each value 
+                        //~ breaks up a single acceleration 
+                        //$ breaks up acceleratrion objects
+                        BlobTheBuilder.Append(x[i].TimeCaptured + "~" + x[i].x + "~"
+                            + x[i].y + "~" + x[i].z + "~" + x[i].location_ID + "$");
+                    }
+                    else
+                    {
+                        //Objects appended to StringBuilder with a $ added to sperate each value 
+                        //~ breaks up a single acceleration 
+                        //$ breaks up acceleratrion objects
+                        BlobTheBuilder.Append(x[i].x + "~" + x[i].y + "~" + x[i].z + "~" + x[i].location_ID + "$");
+                    }
                 }
             }
 
             BlobStringCounter += length + 1;
             //If the StringBuilder is more than 4000 characters push to database and clear the builder 
-            if (BlobStringCounter > 4000 || testFlag == true)
+            if (BlobStringCounter >= MiscellaneousConstants.BLOB_SIZE || testFlag == true)
             {
                 this.Blob = BlobTheBuilder.ToString();
                 Database.DatabaseOperations.AddAccelerationBlobData(this, time, true);
@@ -101,20 +128,50 @@ namespace ControlRoomApplication.Entities
                 //create an empty Acceleration object
                 Acceleration tempAcc = new Acceleration();
 
-                //set the acceleration vector value
-                tempAcc.acc = Convert.ToDouble(accValues[0]);
 
-                //set the acceleration x value
-                tempAcc.x = Convert.ToInt16(accValues[1]);
+                //if this is the first datapoint in the sub blob, it will have the acceleration TimeCaptured value, otherwise the TimeCaptured value will be manually calculated
+                if(i % MiscellaneousConstants.SUB_BLOB_SIZE == 0)
+                {
+                    //set the TimeCaptured value
+                    tempAcc.TimeCaptured = long.Parse(accValues[0]);
 
-                //set the acceleration y value
-                tempAcc.y = Convert.ToInt16(accValues[2]);
+                    //set the acceleration x value
+                    tempAcc.x = Convert.ToInt16(accValues[1]);
 
-                //set the acceleration z value
-                tempAcc.z = Convert.ToInt16(accValues[3]);
+                    //set the acceleration y value
+                    tempAcc.y = Convert.ToInt16(accValues[2]);
 
-                //set the acceleration location ID
-                tempAcc.location_ID = Convert.ToInt16(accValues[4]);
+                    //set the acceleration z value
+                    tempAcc.z = Convert.ToInt16(accValues[3]);
+
+                    //set the acceleration location ID
+                    tempAcc.location_ID = Convert.ToInt16(accValues[4]);
+
+                    //set the acceleration magnitude
+                    tempAcc.acc = Math.Sqrt(tempAcc.x * tempAcc.x + tempAcc.y * tempAcc.y + tempAcc.z * tempAcc.z);
+
+                }
+                else
+                {
+                    //Calculate this datapoints TimeCaptured based on the TimeCaptured of the previous datapoint + the Time Delta
+                    //I need to look into the (long) cast, as it removes precision...
+                    tempAcc.TimeCaptured = (long)(accArray[i - 1].TimeCaptured + MiscellaneousConstants.SAMPLE_TIME_DELTA);
+
+                    //set the acceleration x value
+                    tempAcc.x = Convert.ToInt16(accValues[0]);
+
+                    //set the acceleration y value
+                    tempAcc.y = Convert.ToInt16(accValues[1]);
+
+                    //set the acceleration z value
+                    tempAcc.z = Convert.ToInt16(accValues[2]);
+
+                    //set the acceleration location ID
+                    tempAcc.location_ID = Convert.ToInt16(accValues[3]);
+
+                    //set the acceleration magnitude
+                    tempAcc.acc = Math.Sqrt(tempAcc.x * tempAcc.x + tempAcc.y * tempAcc.y + tempAcc.z * tempAcc.z);
+                }
 
                 //store the parsed acceleration point in the Acceleration[]
                 accArray[i] = tempAcc;
