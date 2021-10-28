bcf33c2ce604b979964d8e981d6e733b96933d0c
YCPjsteck <jsteck@ycp.edu>
2021-10-28 01:05:01 -0400

    Added TCPCommunicationResult class along with a ParseTCPCommandResult enum to allow the RemoteListener class to return descriptive error messages to the mobile user. The TCPCommunicationResult class takes 3 parameters, a MovementResult enum to know first if the command was valid, and then if the movement passed to the PLC succeeded or failed. Next it takes a ParseTCPCommandResult enum which gives a very descriptive result of if the command itself was successfully or unsuccessfully parsed (regardless of if the actual movement succeeded; this is indicated by MovementResult.) Finally, added an optional string parameter which, on error, will return an extremely descriptive error message that is a TCPCommunicationConstant string.

---
+++ ControlRoomApplication/ControlRoomApplication/Controllers/Communications/TCPCommunicationResult.cs
@@ -0,0 +1,27 @@
+﻿using ControlRoomApplication.Controllers.PLCCommunication.PLCDrivers.MCUManager.Enumerations;
+using ControlRoomApplication.Controllers.Communications.Enumerations;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControlRoomApplication.Controllers.Communications
+{
+    public class TCPCommunicationResult
+    {
+        public MovementResult movementResult { get; set; }
+        public ParseTCPCommandResult tcpCommunicationResult { get; set; }
+
+        public String errorMessage { get; set; }
+
+        // Optional ErrMessage param that can be set if an error occurred, This will allow descriptive communication between CR and Mobile
+        public TCPCommunicationResult(MovementResult mvmtResult, ParseTCPCommandResult tcpCommResult, String errMessage = null)
+        {
+            movementResult = mvmtResult;
+            tcpCommunicationResult = tcpCommResult;
+            errorMessage = errMessage;
+
+        }
+    }
+}
