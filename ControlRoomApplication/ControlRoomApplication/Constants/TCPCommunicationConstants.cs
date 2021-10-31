9d44ff9c8d94a3eee445c49684ee26a8cd196e00
DanPalm5 <dpalmieri@ycp.edu>
2021-10-31 01:28:53 -0400

    Created new constants inside TCPCommuncation constants
    - New integers to index the splitCommandString containing parsed values for every command type, in case protocol changes
    - New communication constant strings describing invalid number of parameters for commands
    - New integers to allow for checking of valid number of parameters before parsing occurs

--- ControlRoomApplication/ControlRoomApplication/Constants/TCPCommunicationConstants.cs
+++ ControlRoomApplication/ControlRoomApplication/Constants/TCPCommunicationConstants.cs
@@ -8,6 +8,7 @@ namespace ControlRoomApplication.Constants
 {
     public sealed class TCPCommunicationConstants
     {
+        // Error messages
         public static String VERSION_CONVERSION_ERR = "Error converting version from string to double: ";
 
         public static String VERSION_NOT_FOUND = "The specified version was not found: ";
@@ -20,7 +21,9 @@ namespace ControlRoomApplication.Constants
 
         public static String RELATIVE_MOVE_ERR = "Command RELATIVE_MOVE failed with result: ";
 
-        public static String MISSING_OVERRIDE_ARG = "No override argument supplied for SET_OVERRIDE. ";
+        public static String MISSING_OVERRIDE_ARG = "No sensor name supplied for SET_OVERRIDE. ";
+
+        public static String MISSING_SET_OVERRIDE_ARG = "No true/false value supplied for SET_OVERRIDE.";
 
         public static String INVALID_SENSOR_OVERRIDE = "No valid sensor was found to be overridden in the data string";
 
@@ -31,5 +34,50 @@ namespace ControlRoomApplication.Constants
         public static String MISSING_SENSOR_INIT_ARGS = "SENSOR_INIT command is missing arguments; supply all 10 of them. 1" +
             "means initialize this sensor, 0 means do not initialize.";
         public static String COMMAND_NOT_FOUND = "The specified command was not found or is invalid: ";
+
+        public static String MISSING_SCRIPT_TYPE_ARG = "Missing a script name after 'SCRIPT'";
+
+        public static String MISSING_COMMAND_ARGS = "The total number of arguments is invalid for this type of command ";
+
+
+        // Integers for indexing array of command pieces
+        // General numbers
+        public static int VERSION_NUM = 0;
+        public static int COMMAND_TYPE = 1;
+        public static int MIN_NUM_PARAMS = 3; // case of STOP_RT, will only have Version, stopRT, time --> <V> | STOP_RT | <time>
+
+        // Total params expected
+        public static int NUM_SENSOR_OVERRIDE_PARAMS = 5;
+        public static int NUM_SCRIPT_PARAMS = 4;
+        public static int NUM_ORIENTATION_MOVE_PARAMS, NUM_RELATIVE_MOVE_PARAMS = 5;
+        public static int NUM_ALL_STOP_PARAMS = 3;
+        public static int NUM_SENSOR_INIT_PARAMS = 4;
+        public static int NUM_REQUEST_PARAMS = 4;
+
+        // SET_OVERRIDE specific numbers
+        public static int SENSOR_TO_OVERRIDE = 2;
+        public static int DO_OVERRIDE = 3;
+        public static int SENSOR_OVERRIDE_TIME = 4;
+
+        // SCRIPT specific numbers
+        public static int SCRIPT_NAME = 2;
+        public static int SCRIPT_TIME = 3;
+
+        // ORIENTATION_MOVE specific numbers
+        public static int ORIENTATION_MOVE_AZ = 2;
+        public static int ORIENTATION_MOVE_EL = 3;
+        public static int ORIENTATION_MOVE_TIME = 4;
+
+        // RELATIVE_MOVE specific numbers
+        public static int RELATIVE_MOVE_AZ = 2;
+        public static int RELATIVE_MOVE_EL = 3;
+        public static int RELATIVE_MOVE_TIME = 4;
+
+        // REQUEST specific numbers
+        public static int REQUEST_TYPE = 2;
+        public static int REQUEST_TIME = 3;
+
+        // SENSOR_INIT numbers
+        public static int SENSOR_INIT_VALUES = 2;
     }
 }
